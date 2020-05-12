6f8119819cdeafd62b06acfaa95a89fb4620c310
Yash Deshpande <yashrajas@gmail.com>
2020-05-11 23:52:05 -0400

    Cleaning helper functions

--- helper.py
+++ helper.py
@@ -82,3 +82,66 @@ def compute_iou(box1, box2):
     
     return a.intersection(b).area / a.union(b).area
 
+class GANLoss(nn.Module):
+    """
+    The GANLoss class abstracts away the need to create the target label tensor
+    that has the same size as the input.
+    """
+    def __init__(self, gan_mode, real_label=1.0, gen_label=0.0):
+        """
+        Parameters:
+            gan_mode (str) - - the type of GAN objective. It currently supports vanilla, lsgan, and wgangp.
+            target_real_label (bool) - - label for a real image
+            target_gen_label (bool) - - label of a generated image
+        Note: Do not use sigmoid as the last layer of Discriminator.
+        LSGAN needs no sigmoid. vanilla GANs will handle it with BCEWithLogitsLoss.
+        """
+        super(GANLoss, self).__init__()
+        
+        self.gan_mode = gan_mode
+        self.real_label = real_label
+        self.gen_label = gen_label
+        
+        if gan_mode == 'LS':
+            self.loss = nn.MSELoss().to(device)
+        elif gan_mode == 'BCE':
+            self.loss = nn.BCEWithLogitsLoss().to(device)
+
+    def get_target_tensor(self, output, target_is_real):
+        # Create label tensors with same size as the discriminator output.
+        if target_is_real:
+            if torch.cuda.is_available():
+                target_tensor = torch.cuda.FloatTensor([self.real_label])
+            else:
+                target_tensor = torch.Tensor([self.real_label])
+        else:
+            if torch.cuda.is_available():
+                target_tensor = torch.cuda.FloatTensor([self.gen_label])
+            else:
+                target_tensor = torch.Tensor([self.gen_label])
+                
+        return target_tensor.expand_as(output)
+
+    def __call__(self, output, target_is_real):
+        target_tensor = self.get_target_tensor(output, target_is_real)
+        loss = self.loss(output, target_tensor)
+        return loss
+    
+def init_weights(m):  # define the initialization function
+    classname = m.__class__.__name__
+    if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm2d') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0.0)
+        
+def save_checkpoint(fpath, state):
+    torch.save(state, fpath)
+    
+def print_train_stats(epoch, n_epochs, i, n_batches, loss_d_total, loss_g_total, d_real_mean, d_gen_mean_pre, d_gen_mean_post):
+    print('Epoch: [%d/%d]\tBatch: [%d/%d]\n' % (epoch + 1, n_epochs, i, n_batches),
+          'Loss_D: %.4f\tLoss_G: %.4f\n' % (loss_d_total.item(), loss_g_total.item()),
+          'D(real): %.4f  ||  D(gen) Pre-step: %.4f  ||  D(gen) Post-step: %.4f\n' %
+          (d_real_mean, d_gen_mean_pre, d_gen_mean_post))
+          
+          
